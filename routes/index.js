582682ccdf70162872afb9c2fc9ca0b7f0121cc8
szewah <swgrleong@gmail.com>
2019-08-21 19:01:06 -0400

    setting up routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.send('respond with a resource');
+  res.sendFile(path.join(__dirname, "../views/signup.html"))
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('This is the page for the user');
+  res.sendFile(path.join(__dirname, "../views/form.html"))
 });
 
 module.exports = router;
