04bd2a2c534911fdb1ca1d3ed345285d586bc153
szewah <swgrleong@gmail.com>
2019-08-22 15:30:31 -0400

    adding hash

--- routes/signup.js
+++ routes/signup.js
@@ -9,18 +9,20 @@ router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, "../views/signup.html"))
 });
 
-// router.post('/', async(req, res) => {
-//   console.log(req.body);
-//   db.User.create({
-//     name: req.body.firstName,
-//     lastName: req.body.lastName,
-//     email: req.body.userEmail
-//     password: req.body.userPassword
-//   }).then(results => {
-//     console.log(results);
-//   })
-
-// })
+router.post('/', async(req, res) => {
+  console.log(req.body);
+  db.User.create({
+    name: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.userEmail,
+    password: req.body.userPassword
+  }).then(results => {
+    console.log(results);
+  }).catch((err) => {
+    console.log(err);
+    req.json(err);
+  });
+});
 
 
 module.exports = router;
