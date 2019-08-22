9b1cc0c178dfa6fda43f27500fcc3dc23e98f3de
szewah <swgrleong@gmail.com>
2019-08-22 15:00:30 -0400

    configuering passport

---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models').User;
+
+passport.use(new LocalStrategy (
+        {
+            usernameField: "email"
+        }, 
+            
+        {
+        password, done
+
+        }
+        
+        
+         
+
+    }
+
+))
