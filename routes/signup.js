eb2d3610d2b6c329cfdb31c05a1fb3e2fb4ab453
szewah <swgrleong@gmail.com>
2019-08-22 17:54:53 -0400

    adding hash

--- routes/signup.js
+++ routes/signup.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var path = require('path');
 var db = require('../models');
 
+
 /* GET signup page. */
 router.get('/', function(req, res) {
   // res.send('respond with a resource');
@@ -10,17 +11,19 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', async(req, res) => {
-  console.log(req.body);
-  db.User.create({
-    name: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.userEmail,
-    password: req.body.userPassword
-  }).then(results => {
-    console.log(results);
-  }).catch((err) => {
-    console.log(err);
-    req.json(err);
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    req.flash("signMessage", "You are now registered");
+    db.User.create({
+      name: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.userEmail,
+      password: hash
+    }).then(results => {
+      console.log(results);
+    }).catch((err) => {
+      res.json(err);
+    });
   });
 });
 
